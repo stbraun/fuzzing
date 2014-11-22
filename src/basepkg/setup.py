cbc96f838851cde49c75e86086084e52c0c313c2
sb <sb@action.ms>
2014-11-22 12:47:22 +0100

    Initial package structure.

---
+++ src/basepkg/setup.py
@@ -0,0 +1,55 @@
+'''
+basepkg: Some general meta classes.
+
+Note that "python setup.py test" invokes pytest on the package. With appropriately
+configured setup.cfg, this will check both xxx_test modules and docstrings.
+
+Copyright 2014, Stefan Braun.
+Licensed under MIT.
+'''
+import sys
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+# This is a plug-in for setuptools that will invoke py.test
+# when you run python setup.py test
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest  # import here, because outside the required eggs aren't loaded yet
+        sys.exit(pytest.main(self.test_args))
+
+
+version = "0.1.0"
+
+setup(name="basepkg",
+      version=version,
+      description="Some general meta classes.",
+      long_description=open("README.rst").read(),
+      classifiers=[ # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python'
+      ],
+      keywords="key word", # Separate with spaces
+      author="Stefan Braun",
+      author_email="sb@action.ms",
+      url="http://homepage/url",
+      license="MIT",
+      packages=find_packages(exclude=['examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=['pytest'],
+      cmdclass={'test': PyTest},
+      
+      # TODO: List of packages that this one depends upon:   
+      install_requires=['sphinx'],
+      # TODO: List executable scripts, provided by the package (this is just an example)
+      entry_points={
+        'console_scripts': 
+            ['basepkg=meta:main']
+      }
+)
