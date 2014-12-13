bbbe06ed1bfd51c4e817d46580dfe00193005325
sb <sb@action.ms>
2014-12-13 21:20:25 +0100

    Copied from release branch because of merge issue.

--- src/basepkg/features/fuzzer.feature
+++ src/basepkg/features/fuzzer.feature
@@ -4,21 +4,69 @@ Feature: Provide a fuzz tester.
   a driver feeding data to the SUT (Software Under Test),
   and an oracle detecting success or failure of a test run.
 
+  | Test given applications with fuzzed files. Count successful and failed calls.
+  | Call a given application with a file that was fuzzed before.
+  | Example: Take a jpeg file, fuzz it and call Preview with the fuzzed file.
+
   # The exact number of modified bytes can't be guaranteed.
   # This is because a byte will be replaced with a random value which
   # may be the same as already there.
   # Therefore only the maximum number of modified bytes is known.
   Scenario: Fuzz a binary buffer with minimum number of modifications.
-    Given a bytearray of len 10
+    Given a byte array of len 10
     When feeding it into the fuzzer, setting the fuzz_factor to 10
     Then it will return a buffer with up to two modified bytes.
 
-  Scenario: Fuzz a binary buffer with maximum number of modifications.
-    Given a bytearray of len 10
-    When feeding it into the fuzzer, setting the fuzz_factor to 1
-    Then it will return a buffer with up to 10 modified bytes.
+  Scenario Outline: Fuzz a binary buffer with different numbers of modifications.
+    Given a byte array of len 10
+    When feeding it into the fuzzer, setting the fuzz_factor to <fuzz_factor>
+    Then it will return a buffer with up to <max_modified> modified bytes.
 
-  Scenario: Create a list of fuzzed variants of a given string.
+    Examples:
+    | fuzz_factor | max_modified |
+    |           1 |           10 |
+    |           5 |            3 |
+    |          11 |            2 |
+    |         101 |            2 |
+
+
+  Scenario Outline: Create a list of fuzzed variants of a given string.
     Given a string as seed.
-    When feeding the seed into the fuzzer, providing a count of 5
-    Then it will return a list of 5 fuzzed variants of the seed.
+    """
+      Create fuzzed variants of this string.
+      A test seed for our fuzz tester.
+      Multiple lines are fine.
+      """
+    When feeding the seed into the fuzzer, providing a count of <count>
+    Then it will return a list of <len_list> fuzzed variants of the seed.
+
+    Examples:
+    | count | len_list |
+    |     0 |        0 |
+    |     1 |        1 |
+    |    11 |       11 |
+    |   101 |      101 |
+
+
+  @slow
+  Scenario Outline: File fuzzer
+    Given a list of file paths
+    | file_path              |
+    | ./features/data/t1.pdf |
+    | ./features/data/t2.jpg |
+    | ./features/data/t3.pdf |
+    And a list of applications
+    | application                                                               |
+    | /Applications/Adobe Reader 9/Adobe Reader.app/Contents/MacOS/AdobeReader  |
+    | /Applications/Preview.app/Contents/MacOS/Preview                          |
+    And a FuzzExecutor instance created with those lists.
+    When running a test <runs> times
+    Then a randomly chosen application will be called with a randomly chosen file.
+    And <runs> results are recorded.
+
+    Examples: Test runs
+    | runs |
+    |    0 |
+    |    1 |
+    |    2 |
+    |   11 |
--- src/basepkg/features/steps/ft_fuzzer.py
+++ src/basepkg/features/steps/ft_fuzzer.py
@@ -3,65 +3,177 @@
 
 from behave import *
 
-from gp_tools.fuzzer import fuzzer, fuzz_string
+from gp_tools.fuzzer import fuzzer, fuzz_string, FuzzExecutor
 
 
-@given("a bytearray of len 10")
+@given("a byte array of len 10")
 def step_impl(context):
-    """Prepare a bytearray."""
+    """Prepare a byte array.
+
+    :param context: test context.
+    """
     context.buf = bytearray(10)
 
 
 @when("feeding it into the fuzzer, setting the fuzz_factor to 10")
 def step_impl(context):
-    """Execute fuzzer."""
+    """Execute fuzzer.
+
+    :param context: test context.
+    """
     context.fuzzed_buf = fuzzer(context.buf, 10)
+
 
 @then("it will return a buffer with up to two modified bytes.")
 def step_impl(context):
-    """Check assertions."""
+    """Check assertions.
+
+    :param context: test context.
+    """
     assert len(context.buf) == len(context.fuzzed_buf)
     count = number_of_modified_bytes(context.buf, context.fuzzed_buf)
     assert count < 3
-    assert count > 0
+    assert count >= 0
 
 
-@when("feeding it into the fuzzer, setting the fuzz_factor to 1")
-def step_impl(context):
-    """Execute fuzzer."""
-    context.fuzzed_buf = fuzzer(context.buf, 10)
+@when("feeding it into the fuzzer, setting the fuzz_factor to {fuzz_factor:d}")
+def step_impl(context, fuzz_factor):
+    """Execute fuzzer.
 
-@then("it will return a buffer with up to 10 modified bytes.")
-def step_impl(context):
-    """Check assertions."""
+    :param fuzz_factor: specified fuzz_factor.
+    :param context: test context.
+    """
+    context.fuzzed_buf = fuzzer(context.buf, fuzz_factor)
+
+
+@then("it will return a buffer with up to {max_modified:d} modified bytes.")
+def step_impl(context, max_modified):
+    """Check assertions.
+
+    :param max_modified: maximum expected number of modifications.
+    :param context: test context.
+    """
     assert len(context.buf) == len(context.fuzzed_buf)
     count = number_of_modified_bytes(context.buf, context.fuzzed_buf)
-    assert count > 0
+    assert count >= 0
+    assert count <= max_modified
 
 
 @given("a string as seed.")
 def step_impl(context):
-    """Provide a string."""
-    context.seed = """A test seed for our fuzz tester.
-                      Multiple lines are fine."""
+    """Provide a string.
 
-@when("feeding the seed into the fuzzer, providing a count of 5")
-def step_impl(context):
-    """Execute fuzzer."""
+    :param context: test context.
+    """
+    context.seed = context.text
+
+
+@when("feeding the seed into the fuzzer, providing a count of {count:d}")
+def step_impl(context, count):
+    """Execute fuzzer.
+
+    :param count: number of string variants to generate.
+    :param context: test context.
+    """
     fuzz_factor = 11
-    context.fuzzed_string_list = fuzz_string(context.seed, 5, fuzz_factor)
+    context.fuzzed_string_list = fuzz_string(context.seed, count, fuzz_factor)
 
-@then("it will return a list of 5 fuzzed variants of the seed.")
-def step_impl(context):
-    """Check assertions."""
-    assert len(context.fuzzed_string_list) == 5
+
+@then("it will return a list of {len_list:d} fuzzed variants of the seed.")
+def step_impl(context, len_list):
+    """Check assertions.
+
+    :param len_list: expected number of variants.
+    :param context: test context.
+    """
+    assert len(context.fuzzed_string_list) == len_list
     for fuzzed_string in context.fuzzed_string_list:
         assert len(context.seed) == len(fuzzed_string)
         count = number_of_modified_bytes(context.seed, fuzzed_string)
-        assert count > 0
+        assert count >= 0
 
+# ## file fuzzer
+
+
+@given("a list of file paths")
+def step_impl(context):
+    """Create file list.
+
+    :param context: test context.
+    """
+    assert context.table, "ENSURE: table is provided."
+    context.file_list = [row['file_path'] for row in context.table.rows]
+
+
+@given("a list of applications")
+def step_impl(context):
+    """Create application list.
+
+    :param context: test context.
+    """
+    assert context.table, "ENSURE: table is provided."
+    context.app_list = [row['application'] for row in context.table.rows]
+
+
+@given("a FuzzExecutor instance created with those lists.")
+def step_impl(context):
+    """Create application list.
+
+    :param context: test context.
+    """
+    assert context.app_list and len(context.app_list) > 0, "ENSURE: app list is provided."
+    assert context.file_list and len(context.file_list) > 0, "ENSURE: file list is provided."
+    context.fuzz_executor = FuzzExecutor(context.app_list, context.file_list)
+    assert context.fuzz_executor, "VERIFY: fuzz executor created."
+
+
+@when("running a test {runs:d} times")
+def step_impl(context, runs):
+    """Execute multiple runs.
+
+    :param runs: number of test runs to perform.
+    :param context: test context.
+    """
+    executor = context.fuzz_executor
+    executor.run_test(runs)
+    assert sum(executor.stats.values()) == runs, "VERIFY: stats available."
+
+
+@then("a randomly chosen application will be called with a randomly chosen file.")
+def step_impl(context):
+    """Check called apps / files.
+
+    :param context: test context.
+    """
+    executor = context.fuzz_executor
+    pairs = executor.test_pairs
+    app_2_files = {}
+    for app, file in pairs:
+        if app not in app_2_files:
+            app_2_files[app] = []
+        if file not in app_2_files[app]:
+            app_2_files[app].append(file)
+    if len(pairs) > 10 and len(context.file_list) > 1:
+        multiple_files = False
+        for app, files in app_2_files.items():
+            multiple_files = context.file_list or len(files) > 0
+        assert multiple_files, "VERIFY: at least one app was tested with multiple files."
+
+
+@then("{runs:d} results are recorded.")
+def step_impl(context, runs):
+    """Check called apps / files.
+
+    :param runs: expected number of records.
+    :param context: test context.
+    """
+    executor_ = context.fuzz_executor
+    assert sum(executor_.stats.values()) == runs, "VERIFY: Number of recorded runs."
+    for app, count in executor_.stats.items():
+        assert count > 0, "VERIFY: at least one test must have been performed and recorded."
 
 # ##### helpers
+
 
 def number_of_modified_bytes(buf, fuzzed_buf):
     """Determine the number of differing bytes.
