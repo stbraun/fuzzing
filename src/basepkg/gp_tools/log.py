0f5aac71577f62e7e55b38ceeebcde67858a97f6
sb <sb@action.ms>
2014-12-18 21:11:18 +0100

    basepkg-29 Provide easy to configure and use logger.

---
+++ src/basepkg/gp_tools/log.py
@@ -0,0 +1,53 @@
+# coding=utf-8
+"""Logging support.
+
+More convenient logging support built on top of logging package.
+"""
+
+import logging
+import logging.config
+import pkgutil
+import yaml
+
+from gp_decorators.singleton import singleton
+
+
+@singleton
+class LoggerFactory(object):
+    """Create and manage logger instances.
+
+    It is important to initialize the logging framework before
+    the first call to a logger.
+    """
+    def __init__(self, package_name='gp_tools', config_file='resources/log_config.yaml'):
+        self.package_name = package_name
+        self.config_file = config_file
+        self.config = None
+
+    def initialize(self):
+        """Initialize logging framework.
+
+        Must be called before first logging attempt!
+        """
+        self.config = self.__read_configuration()
+        logging.config.dictConfig(self.config)
+
+    @staticmethod
+    def get_instance(identifier):
+        """Get a logger instance.
+
+        :param identifier: identifier of logger as addressed in configuration.
+        :return: the requested logger.
+        """
+        logger = logging.getLogger(identifier)
+        return logger
+
+    def __read_configuration(self):
+        """Read the logging configuration from file.
+
+        :return: configuration dictionary
+        :rtype: dict
+        """
+        cfg = pkgutil.get_data(self.package_name, self.config_file)
+        conf_dict = yaml.load(cfg)
+        return conf_dict
