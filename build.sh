a8751c7770f01301603e2f20a21bd3f6314db405
sb <sb@action.ms>
2018-05-07 22:05:41 +0200

    move all into build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,20 @@
 
 source venv/bin/activate
 
+rm -rf .coveraage
+rm -rf reports
+mkdir reports
+rm -rf venv
+
+python3 -m venv venv
+
 pip install -r requirements.txt
 
 flake8 --output-file reports/flake8.txt --benchmark --count --statistics fuzzing run_fuzzer.py
 
-nosetests --with-coverage --cover-branches --cover-inclusive --cover-html --cover-html-dir=reports/coverage --cover-xml --cover-xml-file=reports/coverage.xml tests/
+pylint --rcfile=resrc/pylintrc fuzzing > reports/pylint.txt
+
+nosetests --with-coverage --cover-branches --cover-inclusive --with-xunit --xunit-file=reports/nosetests.xml --cover-html --cover-html-dir=reports/coverage --cover-xml --cover-xml-file=reports/coverage.xml tests/
 
 paver sdist
 
