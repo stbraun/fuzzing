eda7c9fc69850495ec7985512df5a6643158da7d
sb <sb@action.ms>
2018-05-08 18:27:14 +0200

    cleanup build-sh

--- build.sh
+++ build.sh
@@ -1,25 +1,35 @@
 #usr/bin/env bash
 
-source venv/bin/activate
-
-rm -rf .coveraage
-rm -rf reports
+# prepare folder for build reports
 mkdir reports
-rm -rf venv
 
+# setup virtual environment ...
 python3 -m venv venv
 
+# ... and activate it
+source venv/bin/activate
+
+# install required packages
 pip install -r requirements.txt
 
+# run sanity checks
 flake8 --output-file reports/flake8.txt --benchmark --count --statistics fuzzing run_fuzzer.py
 
 pylint --rcfile=resrc/pylintrc fuzzing > reports/pylint.txt
 
+# run test and measure coverage
 nosetests --with-coverage --cover-branches --cover-inclusive --with-xunit --xunit-file=reports/nosetests.xml --cover-html --cover-html-dir=reports/coverage --cover-xml --cover-xml-file=reports/coverage.xml tests/
 
+# build source distribution tarball
 paver sdist
 
+# install package ...
 python setup.py install
 
+# ... and generate documentation
 cd docs
 make html
+
+# package documentation
+cd build
+zip -r ../../dist/docs.zip html
