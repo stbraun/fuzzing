1143284f446f46652ca676a2cb7729a5134ea5ba
sb <sb@action.ms>
2018-05-12 14:12:38 +0200

    flake8 issues fixed

--- run_fuzzer.py
+++ run_fuzzer.py
@@ -4,29 +4,29 @@
 
 Copyright (c) 2015 Stefan Braun
 
-Permission is hereby granted, free of charge, to any person obtaining a copy 
+Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and
-associated documentation files (the "Software"), to deal in the Software 
+associated documentation files (the "Software"), to deal in the Software
 without restriction,
-including without limitation the rights to use, copy, modify, merge, 
+including without limitation the rights to use, copy, modify, merge,
 publish, distribute,
-sublicense, and/or sell copies of the Software, and to permit persons to 
+sublicense, and/or sell copies of the Software, and to permit persons to
 whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in 
+The above copyright notice and this permission notice shall be included in
 all copies or
 substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
 A PARTICULAR PURPOSE
-AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
 LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
@@ -82,7 +82,7 @@ def validate_config(conf_dict):
     """
     # TASK improve validation
     if APPLICATIONS not in conf_dict.keys() or SEED_FILES not in \
-        conf_dict.keys():
+            conf_dict.keys():
         raise InvalidConfigurationError
     if RUNS not in conf_dict.keys():
         conf_dict[RUNS] = DEFAULT_RUNS
