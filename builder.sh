ff9de753730f69de2ffa2df3a4b563d776501f64
sb <sb@action.ms>
2018-07-13 07:56:35 +0200

    improve builder.sh for Jenkins

--- builder.sh
+++ builder.sh
@@ -72,7 +72,7 @@ check_sources() {
 
 run_tests() {
     # run test and measure coverage
-    if nosetests --with-coverage --cover-branches --cover-inclusive --with-xunit --xunit-file=reports/nosetests.xml --cover-html --cover-html-dir=reports/coverage --cover-xml --cover-xml-file=reports/coverage.xml tests/  > reports/nosetest.txt 2>&1; then
+    if nosetests --cover-package=fuzzing --with-coverage --cover-branches --cover-inclusive --with-xunit --xunit-file=reports/nosetests.xml  --cover-xml --cover-xml-file=reports/coverage.xml tests/ ; then
         echo "===================";
         echo " unit tests passed ";
         echo "===================";
